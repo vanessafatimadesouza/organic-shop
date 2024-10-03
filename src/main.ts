abb4e02c8cdd9043aa2418d116b463d9585dc2a9
Vanessa de Souza <124921663+vanessafatimadesouza@users.noreply.github.com>
2024-10-03 10:38:53 -0300

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
+
+platformBrowser()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err))
