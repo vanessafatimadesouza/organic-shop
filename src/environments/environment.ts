41a2d53896d46a9b75f022ad527639055185f77a
Vanessa de Souza <124921663+vanessafatimadesouza@users.noreply.github.com>
2024-10-03 11:00:12 -0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyDp9VaVDHFVdkTVbsR9H6mEz7DPlzY_7Lk",
+        authDomain: "oshop-5f88b.firebaseapp.com",
+        projectId: "oshop-5f88b",
+        storageBucket: "oshop-5f88b.appspot.com",
+        messagingSenderId: "981654391080",
+        appId: "1:981654391080:web:243a28929eff8a4917281d"
+    }
+}
